a61a508b7cecfa5f97567ab62fac812d888d0dc4
MarkMegarry <30323234+MarkMegarry@users.noreply.github.com>
2019-03-04 11:01:18 +0000

    Update main.py
    Checks up to 100 blocks beneath the player, as opposed to checking up to the block at y=-100.  The previous method exhibited varied execution time based on how many blocks were below the player.  If bedrock is checked, loop ends early.

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ start  = time.time()
 #MCPi setup
 mc=Minecraft.create()
 pos = mc.player.getPos()
-#getPos stores coordinates in a Vec3 object, rather than a vector
+#getPos stores coordinates in a Vec3 object
 #Retrieve values with pos.x, pos.y, pos.z etc.
 #Check blocks directly below player, stop at bedrock
-for y in range(int(pos.y),-120, -1):
+for y in range(int(pos.y),int(pos.y)-100, -1):
     blockType = mc.getBlock(pos.x,y,pos.z)
      #Barrier to entry, check next block if not target block
     if blockType == 15 or blockType == 56 or blockType == 7:
     
-         #Iron condition
+        #Iron condition
         if blockType == 15:
             mc.postToChat("Iron ore beneath you!")
         #Diamond condition
@@ -21,9 +21,12 @@ for y in range(int(pos.y),-120, -1):
             mc.postToChat("Diamond ore beneath you!")
         #Bedrock condition
         else:
+            #Break out of for loop
             break
     print(blockType)
 print("Done!")
 #Print execution time
 end = time.time()
 print("Execution time:" + str(end-start))
+
+
